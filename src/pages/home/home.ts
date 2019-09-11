a74df11ff32071ede37ad1fbd8dd5fd3a00238f8
nvdinh185 <nvdinh0905671805@gmail.com>
2019-09-11 08:40:04 +0700

    commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+
 import { SlidesPage } from '../slides/slides';
 import { CardPage } from '../cards/card/card';
 import { HeaderPage } from '../cards/header/header';
@@ -15,41 +15,13 @@ import { AdvancedWeatherPage } from '../cards/advanced-weather/advanced-weather'
   templateUrl: 'home.html'
 })
 export class HomePage {
-  constructor(public navCtrl: NavController) { }
-
-  slides(){
-    this.navCtrl.push(SlidesPage)
-  }
-
-  card(){
-    this.navCtrl.push(CardPage)
-  }
-
-  header(){
-    this.navCtrl.push(HeaderPage)
-  }
-
-  background(){
-    this.navCtrl.push(BackgroundPage)
-  }
-
-  image(){
-    this.navCtrl.push(ImagePage)
-  }
-
-  list(){
-    this.navCtrl.push(ListPage)
-  }
-
-  advancedMap(){
-    this.navCtrl.push(AdvancedMapPage)
-  }
-
-  advancedSocial(){
-    this.navCtrl.push(AdvancedSocialPage)
-  }
-
-  advancedWeather(){
-    this.navCtrl.push(AdvancedWeatherPage)
-  }
+  SlidesPage = SlidesPage;
+  CardPage = CardPage;
+  HeaderPage = HeaderPage;
+  BackgroundPage = BackgroundPage;
+  ImagePage = ImagePage;
+  ListPage = ListPage;
+  AdvancedMapPage = AdvancedMapPage;
+  AdvancedSocialPage = AdvancedSocialPage;
+  AdvancedWeatherPage = AdvancedWeatherPage;
 }
--- src/pages/slides/slides.ts
+++ src/pages/slides/slides.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
-
+import { Observable } from 'rxjs/Observable';
+import { Events } from 'ionic-angular';
 
 @Component({
   templateUrl: 'slides.html'
 })
 export class SlidesPage {
+  constructor(private events: Events) { }
+  ngOnInit() {
+    this.getObservable().subscribe(data => {
+      console.log('Observerable sau 5 giay: ', data);
+    })
+
+    this.events.subscribe("sent_to_demo", ((data) => {
+      console.log("Nhan tu form demo: " + JSON.stringify(data));
+    }));
+  }
+
   slides = [
     {
       title: "Welcome to the Docs!",
@@ -22,4 +34,20 @@ export class SlidesPage {
       image: "assets/img/ica-slidebox-img-3.png",
     }
   ];
+
+  getObservable() {
+    let observable = new Observable(observer => {
+      setTimeout(() => {
+        observer.next({
+          room_id: 'testID',
+          room_name: 'testName',
+        });
+      }, 5000);
+    });
+    return observable;
+  }
+
+  clickMe() {
+    this.events.publish("sent_to_demo", { data: 'chat' });
+  }
 }
